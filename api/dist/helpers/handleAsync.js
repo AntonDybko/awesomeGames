3578cf8d2de561acc40173e11f09f5d5de25dd65
AntonDybko <dybko.anton11@ukr.net>
2023-10-31 03:32:39 +0100

    gitignore_update
    dist ignore

--- api/dist/helpers/handleAsync.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function handleAsync(fn) {
-    return (req, res, next) => {
-        fn(req, res, next).catch(next);
-    };
-}
-exports.default = handleAsync;
