41b1fa5f0c48476c3e7d8979d62fd0ccff578d72
Michał Kaszubowski <michal.mateusz.kaszubowski@gmail.com>
2023-12-06 15:54:25 +0100

    Fixed no user cookie error. Not optimal

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,13 +2,21 @@ import React from 'react';
 import './App.scss';
 import Navbar from './components/navigation/Navbar';
 import { Routing } from './Routing';
+import { CookiesProvider, useCookies } from "react-cookie";
 
 const App: React.FC = () => {
+  const [cookies, setCookie] = useCookies(["user"]);
+  if (!cookies.user) {
+    setCookie('user', {}, { path: '/' })
+  }
+
   return (
-    <div className="App">
-      <Navbar />
-      <Routing />
-    </div>
+    <CookiesProvider>
+      <div className="App">
+        <Navbar />
+        <Routing />
+      </div>
+    </CookiesProvider>
   );
 }
 
