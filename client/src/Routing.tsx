02680d3cd51fb6fa30520999bb81d17a406f21cd
Michał Kaszubowski <michal.mateusz.kaszubowski@gmail.com>
2024-01-21 21:28:07 +0100

    Clean up

--- client/src/Routing.tsx
+++ client/src/Routing.tsx
@@ -4,7 +4,7 @@ import Games from 'components/games/Games';
 import Ranking from 'components/ranking/Ranking';
 import Profile from 'components/profile/Profile';
 import MastermindPage from 'components/mastermind-page/MastermindPage';
-import Mastermind from 'components/mastermind/Mastermind';
+// import Mastermind from 'components/mastermind/Mastermind';
 import TicTacToePage from 'components/tictactoe-page/TicTacToePage';
 import TicTacToe from 'components/tictactoe/TicTacToe';
 import SessionPersistence from "components/other/SessionPersistence";
@@ -20,7 +20,7 @@ export const Routing: React.FC = () => {
           <Route path="/games" element={<Games />} />
           <Route path="/games/mastermind" element={<MastermindPage />} />
           <Route path="/games/statki" element={<StatkiPage />} />
-          <Route path="/games/mastermind/:id" element={<Mastermind />} />
+          {/* <Route path="/games/mastermind/:id" element={<Mastermind />} /> */}
           <Route path="/games/tictactoe" element={<TicTacToePage />} />
           <Route path="/games/tictactoe/:id" element={<TicTacToe />} />
           <Route path="/ranking" element={<Ranking />} />
