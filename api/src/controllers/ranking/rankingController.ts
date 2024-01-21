bcc849c21951ad6e6094c5e77c6c74a8f8f94268
Michał Kaszubowski <michal.mateusz.kaszubowski@gmail.com>
2024-01-21 22:24:52 +0100

    Fixed ranking getter

--- api/src/controllers/ranking/rankingController.ts
+++ api/src/controllers/ranking/rankingController.ts
@@ -7,26 +7,54 @@ dotenv.config();
 const rankingController = {
     getRanking: async (req: Request, res: Response) => {
         try {
-            const game = await Game.find({ name: req.params.gamename });
-            console.log('Game:', game);
+            const game = await Game.find({ name: req.params.gamename }).exec();
 
-            if (game) {
-                const scores = await Score.aggregate([{
-                    $match: { game: game[0]._id },
-                }, {
-                    $group: {
-                        _id: '$user',
-                        avrageScore: { $avg: '$score' }
+            if (game.length > 0) {
+                const scores = await Score.aggregate([
+                    {
+                        $match: { game: game[0]._id },
                     },
-                }]).exec();
+                    {
+                        $group: {
+                            _id: '$user',
+                            averageScore: { $avg: '$score' },
+                        },
+                    },
+                    {
+                        $sort: { averageScore: -1 },
+                    },
+                    {
+                        $limit: 10,
+                    },
+                    {
+                        $lookup: {
+                            from: 'users', // Name of the User collection
+                            localField: '_id',
+                            foreignField: '_id',
+                            as: 'userDetails',
+                        },
+                    },
+                    {
+                        $unwind: '$userDetails', // Unwind the array created by $lookup
+                    },
+                    {
+                        $project: {
+                            _id: 1,
+                            username: '$userDetails.username',
+                            averageScore: 1,
+                        },
+                    },
+                ]).exec();
 
                 return res.status(200).json(scores);
             }
-            return res.status(404).json({message: "No game with spacified name!"});
+
+            return res.status(404).json({ message: "No game with specified name!" });
         } catch (error) {
-            return res.status(400).json(error);
+            console.error('Error:', error);
+            return res.status(400).json({ message: 'Error processing request' });
         }
-    }
-}
+    },
+};
 
 export default rankingController;
