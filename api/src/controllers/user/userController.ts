5cf6b6f0f39f69a155649aedf5cfa2dc5d0ce485
Anton <dybko.anton11@ukr.net>
2024-06-16 21:59:41 +0200

    editProfie encryption fix

--- api/src/controllers/user/userController.ts
+++ api/src/controllers/user/userController.ts
@@ -23,7 +23,6 @@ const checkIfUnique = async (req: Request, res: Response, field: string) => {
 };
 
 const usersController = {
-    //just for development purposes, delete it or remove later
     getUsers: async (req: Request, res: Response) => {
         const users = await User.find();
         res.json(users);
@@ -44,24 +43,38 @@ const usersController = {
         const userToUpdate = req.body;
 
         if(req.body.password) {
-            if(!validPasswordFormat(req.body.password)){
-                return res.status(400).json({ message: 'Validation failed', details: "Wrong password format.", "criteria": [
-                    "Contains at least one lowercase letter.",
-                    "Contains at least one uppercase letter.",
-                    "Contains at least one digit (0-9).",
-                    "Contains at least one special character from the set [@ $ ! % * ? & _].",
-                    "Consists of only the allowed characters (letters, digits, and specified special characters)."
-                ]});
-            }
-        }
+            try{
+                if(!validPasswordFormat(req.body.password)){
+                    return res.status(400).json({ message: 'Validation failed', details: "Wrong password format.", "criteria": [
+                        "Contains at least one lowercase letter.",
+                        "Contains at least one uppercase letter.",
+                        "Contains at least one digit (0-9).",
+                        "Contains at least one special character from the set [@ $ ! % * ? & _].",
+                        "Consists of only the allowed characters (letters, digits, and specified special characters)."
+                    ]});
+                }
 
-        const user = await User.findOneAndUpdate(
-            { _id: request.user._id },
-            { ...userToUpdate },
-            { new: true },
-        );
-        const { password, refreshToken, _id, __v, ...userWithoutPassword } = JSON.parse(JSON.stringify(user));
-        res.json(userWithoutPassword);
+                const encryptedPass = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10));
+                
+                const { password, ...userToUpdateWithoutPassword } = userToUpdate;
+
+                const user = await User.findOneAndUpdate(
+                    { _id: request.user._id },
+                    { ...userToUpdateWithoutPassword, password: encryptedPass },
+                    { new: true },
+                );
+                const { password: hashedPassword, refreshToken, _id, __v, ...userWithoutPassword } = JSON.parse(JSON.stringify(user));
+                res.json(userWithoutPassword);
+            }catch(err) {console.log(err)}
+        }else{
+            const user = await User.findOneAndUpdate(
+                { _id: request.user._id },
+                { ...userToUpdate },
+                { new: true },
+            );
+            const { password, refreshToken, _id, __v, ...userWithoutPassword } = JSON.parse(JSON.stringify(user));
+            res.json(userWithoutPassword);
+        }
     },
 
     changePassword: async (req: Request, res: Response) => {
