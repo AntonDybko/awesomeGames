4bd000ef2379ccdb46b405ccf89e957a2d1c9668
fwozniakk <45725081+fwozniakk@users.noreply.github.com>
2024-06-19 20:13:28 +0200

    fix(api): fix types

--- api/src/app.ts
+++ api/src/app.ts
@@ -20,7 +20,7 @@ app.use(express.json({limit: '50mb'}));
 app.use("/users", users);
 app.use("/games", games);
 app.use("/ranking", rankings);
-app.use((req, res, next) => {
+app.use((req: Request, res: Response, next: NextFunction) => {
     res.status(404).send('Route not Found');
 });
 
